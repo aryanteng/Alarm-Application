8092b3b0843093e81e569126577798f379314818
aryanteng <aryanteng892@gmail.com>
2023-02-17 04:32:14 +0530

    broadcast receiver for battery okay and battery low implemented and they stop service

--- app/src/main/java/com/example/alarmapplication/MainActivity.kt
+++ app/src/main/java/com/example/alarmapplication/MainActivity.kt
@@ -1,19 +1,31 @@
 package com.example.alarmapplication
 
 import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.alarmapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var batteryLowReceiver: BatteryLowReceiver
+    private lateinit var batteryOkayReceiver: BatteryOkayReceiver
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
+        batteryLowReceiver = BatteryLowReceiver()
 
+        IntentFilter(Intent.ACTION_BATTERY_LOW).also {
+            registerReceiver(batteryLowReceiver, it)
+        }
+
+        IntentFilter(Intent.ACTION_BATTERY_OKAY).also {
+            registerReceiver(batteryOkayReceiver, it)
+        }
 
         binding.btnStart.setOnClickListener {
             val intent = Intent(this, AlarmService::class.java)
